c30640d41c120ef4770f58811975107a237e17fc
Eugene <eshereverov@gmail.com>
2019-04-26 11:45:14 +0300

    create user functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
+// load User model
+const User = require('../../models/User');
+
+// @route api/users/test
+// @desc Test users route
+// @access Public
 router.get('/test', (req, res) => {
   res.json({msg: 'Users works'})
+});
+
+// @route api/users/register
+// @desc Register users
+// @access Public
+router.post('/register', (req, res) => {
+  User
+    .findOne({email: req.body.email})
+    .then((user) => {
+      if(user) {
+        res.status(400).json({email: 'Email already exists'});
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', // size
+          r: 'pg', // raiting
+          d: 'mm', // default
+        })
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar: avatar,
+          password: req.body.password,
+        })
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => res.json(user))
+              .catch((err) => console.log(err));
+          })
+        })
+      }
+    })
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,7 +8,11 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-const db = require('./config/keys').mongoURI
+// body-parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
 
 mongoose
   .connect(db, {
@@ -16,7 +21,7 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'))
+app.get('/', (req, res) => res.send('Hello'));
 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
