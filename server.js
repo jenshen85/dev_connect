128313b9dca8a9e3911443eec532a8bf472052c0
Eugene <eshereverov@gmail.com>
2019-04-26 10:24:37 +0300

    add routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 const db = require('./config/keys').mongoURI
@@ -12,9 +16,11 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  return res.send('Hello')
-})
+app.get('/', (req, res) => res.send('Hello'))
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
