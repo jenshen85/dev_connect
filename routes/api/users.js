3adc28fedbd2b6dccc4b3a62059497cb848d4866
Eugene <eshereverov@gmail.com>
2019-04-26 22:11:29 +0300

    begin validate data from client

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,8 @@ const passport = require('passport');
 
 const secretKey = require('../../config/keys').secretOrKey;
 
+const validateRegisterInput = require('../../validation/register');
+
 // load User model
 const User = require('../../models/User');
 
@@ -21,11 +23,18 @@ router.get('/test', (req, res) => {
 // @desc Register users
 // @access Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  // check validation
+  if(!isValid) {
+    return res.status(400).json(errors)
+  }
+
   User
     .findOne({email: req.body.email})
     .then((user) => {
       if(user) {
-        res.status(400).json({email: 'Email already exists'});
+        errors.email = 'Email already exists'
+        res.status(400).json(errors);
       } else {
         const avatar = gravatar.url(req.body.email, {
           s: '200', // size
