da00ba3bafb6a406e1d3b38dee35f3a5796c2f0d
Eugene <eshereverov@gmail.com>
2019-05-01 18:11:25 +0300

    add and remove comment routes for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -124,4 +124,53 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
   });
 });
 
+// @route POST api/post/comment/:id
+// @desc Add comment to post
+// @access Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  Post.findById(req.params.id)
+    .then((post) => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id,
+      };
+
+      // Add comment to array
+      post.comments.unshift(newComment);
+      // Save
+      post.save().then((post) => res.json(post));
+    })
+    .catch(() => res.status(404).json({ postnotfound: 'Post not found' }));
+});
+
+// @route DELETE api/post/comment/:id/:comment_id
+// @desc Remove comment from post
+// @access Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      // check to see if comment exist
+      if (post.comments.filter((comment) => comment._id.toString() === req.params.comment_id).length === 0) {
+        return res.status(404).json({ commentnotexists: 'Comment does not exist' });
+      }
+
+      // Get remove index
+      const removedIndex = post.comments.map((item) => item._id.toString()).indexOf(req.params.comment_id);
+      // Splice from array
+      post.comments.splice(removedIndex, 1);
+      // Save
+      post.save().then((post) => res.json(post));
+    })
+    .catch(() => res.status(404).json({ postnotfound: 'Post not found' }));
+});
+
 module.exports = router;
