0d17300ba22ddea8eae8757df21d259f723065af
Eugene <eshereverov@gmail.com>
2019-05-01 21:54:00 +0300

    add react router for react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/layout/navbar';
-import Landing from './components/layout/landing';
 import Footer from './components/layout/footer';
+import Landing from './components/layout/landing';
+import Register from './components/auth/register';
+import Login from './components/auth/login';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route path="/" component={Landing} exact />
+        <div className="container">
+          <Route path="/register" component={Register} exact />
+          <Route path="/login" component={Login} exact />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
