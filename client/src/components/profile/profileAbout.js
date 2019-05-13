71ed770227f46cfada2a02f22e37ae5e12c01ce5
Eugene <eshereverov@gmail.com>
2019-05-13 23:20:16 +0300

    add education and experience info to developer profile

--- client/src/components/profile/profileAbout.js
+++ client/src/components/profile/profileAbout.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import isEmpty from '../../validation/is-empty';
 // import PropTypes from 'prop-types';
 
 export class ProfileAbout extends Component {
   render() {
-    return <div>Profile About</div>;
+    const { profile } = this.props;
+    const firstName = profile.user.name.split(' ')[0];
+
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">{firstName}'s Bio</h3>
+            <p className="lead">
+              {isEmpty(profile.bio) ? (
+                <span>{firstName} does not have a bio</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
+            </p>
+            <hr />
+            <h3 className="text-center text-info">Skill Set</h3>
+            <div className="row">
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
+                {isEmpty(profile.skills)
+                  ? null
+                  : profile.skills.map((skill) => (
+                      <div className="p-3" key={skill}>
+                        <i className="fa fa-check" /> {skill}
+                      </div>
+                    ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- client/src/components/profile/profileCreds.js
+++ client/src/components/profile/profileCreds.js
@@ -1,9 +1,76 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
 // import PropTypes from 'prop-types';
 
 export class ProfileCreds extends Component {
   render() {
-    return <div>Profile Creds</div>;
+    const { education, experience } = this.props;
+    const expItems = experience.map((exp) => (
+      <li className="list-group-item" key={exp._id}>
+        <h4>{exp.company}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+          {exp.to === null ? ' Now' : <Moment format="YYYY/MM/DD">exp.to</Moment>}
+        </p>
+        <p>
+          <strong>Position:</strong> {exp.title}
+        </p>
+        <p>
+          {exp.location === null ? null : (
+            <span>
+              <strong>Location:</strong> {exp.location}
+            </span>
+          )}
+        </p>
+        <p>
+          {exp.description === null ? null : (
+            <span>
+              <strong>Description:</strong> {exp.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
+
+    const eduItems = experience.map((edu) => (
+      <li className="list-group-item" key={edu._id}>
+        <h4>{edu.school}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
+          {edu.to === null ? ' Now' : <Moment format="YYYY/MM/DD">edu.to</Moment>}
+        </p>
+        <p>
+          <strong>Degree:</strong> {edu.degree}
+        </p>
+        <p>
+          {edu.fieldofstudy === null ? null : (
+            <span>
+              <strong>Field Of Study:</strong> {edu.fieldofstudy}
+            </span>
+          )}
+        </p>
+
+        <p>
+          {edu.description === null ? null : (
+            <span>
+              <strong>Description:</strong> {edu.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
+    return (
+      <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Experience</h3>
+          <ul className="list-group">{expItems}</ul>
+        </div>
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Education</h3>
+          <ul className="list-group">{eduItems}</ul>
+        </div>
+      </div>
+    );
   }
 }
 
