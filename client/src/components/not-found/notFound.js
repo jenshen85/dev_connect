68bd6d960a6263361b5a50e97f13086936bba365
Eugene <eshereverov@gmail.com>
2019-05-14 09:30:41 +0300

    add not found page

---
+++ client/src/components/not-found/notFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//TODO: make rote for not found page
+
+function NotFound() {
+  return (
+    <div>
+      <h1 className="display-4">Page Not Found</h1>
+      <p>Sorry, this page does not exist</p>
+    </div>
+  );
+}
+
+export default NotFound;
