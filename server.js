881caf9d789f9baf204371de40014d3834540460
Eugene <eshereverov@gmail.com>
2019-05-21 00:46:46 +0300

    loging server

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 // server static assets if in production
+console.log(process.env.NODE_ENV === 'production');
+
 if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static('client/build'));
